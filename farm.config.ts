54c28a1dee84c811ecea3c123ea538fdf2c663f4
Rahman Nurudin <rahwisdilfiqrak@gmail.com>
2025-08-10 17:25:24 +0700

    feat(config): add compilation settings with path alias

--- farm.config.ts
+++ farm.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from "@farmfe/core";
 import farmPostcssPlugin from "@farmfe/js-plugin-postcss";
 
+import path from "path";
+
 export default defineConfig({
 	plugins: ["@farmfe/plugin-react", farmPostcssPlugin()],
 	server: { port: 3000 },
+	compilation: {
+		external: ["node:fs"],
+		resolve: {
+			alias: {
+				"~/": path.join(process.cwd(), "src/"),
+			},
+		},
+	},
 });

9ea6b1801200000598c83a0850c6813fa022ed92
Rahman Nurudin <rahwisdilfiqrak@gmail.com>
2025-08-14 23:00:23 +0700

    refactor(config): remove importMeta plugin and update FARM_API_URL handling

--- src/types/global/env.d.ts
+++ src/types/global/env.d.ts
@@ -1,7 +1,12 @@
-interface ImportMetaEnv {
-	readonly FARM_API_URL: string;
-}
+// declare global {
+// 	interface ImportMetaEnv {
+// 		readonly FARM_API_URL: string;
+// 	}
+// 	const FARM_API_URL
+// }
 
-interface ImportMeta {
-	readonly env: ImportMetaEnv;
-}
+// interface ImportMeta {
+// 	readonly env: ImportMetaEnv;
+// }
+
+declare const FARM_API_URL: string;

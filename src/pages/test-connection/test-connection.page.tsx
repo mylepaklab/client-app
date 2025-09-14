a5275b9ac96ba3019da4c8eef36a577dbe5caa13
Rahman Nurudin <rahwisdilfiqrak@gmail.com>
2025-09-14 21:54:15 +0700

    feat: comment out HandPOC import and usage in TestConnectionPage

--- src/pages/test-connection/test-connection.page.tsx
+++ src/pages/test-connection/test-connection.page.tsx
@@ -1,5 +1,5 @@
-import { HandPOC } from "~/components/HandPOC";
+// import { HandPOC } from "~/components/HandPOC";
 
 export function TestConnectionPage() {
-	return <HandPOC />;
+	return <></>;
 }
